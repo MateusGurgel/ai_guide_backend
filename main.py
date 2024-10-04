acad81b21b8f0a152ba0418ca0d73f7134af6772
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-10-04 13:18:12 -0300

    feat: add a index view

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from modules.study_plan.study_plan_services import StudyPlanServices
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def main_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+class Item(BaseModel):
+    topic: str
 @app.post("/search")
-def read_root(user_message: str):
-    books = StudyPlanServices.get_study_plan(user_message)
+def read_root(request: Item):
+    books = StudyPlanServices.get_study_plan(request.topic)
 
     return {"response": books}
