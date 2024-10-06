6b7d2730d304780cf8a26163e76d5bd5b7f3d0f4
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-10-06 19:08:06 -0300

    feat: add .env port

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from modules.study_plan.study_plan_services import StudyPlanServices
+from decouple import config #type: ignore
+
+PORT = int(config("PORT", default=4000, cast=int)) #type: ignore
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 def main_page(request: Request):
@@ -23,4 +27,4 @@ def read_root(request: Item):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, port=PORT)
